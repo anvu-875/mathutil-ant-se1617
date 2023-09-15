260fb523c07fc01d9fd3fdbc7f93aa8a5d6bec94
An Vu <hoanganwm@gmail.com>
2023-09-15 14:42:02 +0700

    update success

--- src/com/wm/mahtutil/core/MathUtility.java
+++ src/com/wm/mahtutil/core/MathUtility.java
@@ -23,7 +23,7 @@ public class MathUtility {
         }
         if (n == 0 || n == 1) return 1;
         long product = 1;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             product *= i;
         }
         return product;
